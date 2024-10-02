d9e5a1af7e560da28b4aecbc681681cb11dd69df
pedreca <80847947+ArthurPedreca@users.noreply.github.com>
2024-10-02 07:31:44 -0300

    Revert "get location user"

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,31 +1,73 @@
-import { useEffect } from 'react';
+
+import { Link } from 'react-router-dom'
 
 function Home() {
-    
-    useEffect(() => {
-        // Função para buscar o IP e as informações de geolocalização
-        const fetchGeoLocation = async () => {
-            try {
-                // Primeiro, busca o IP do usuário
-                const ipResponse = await fetch('https://api.ipify.org');
-                const ip = await ipResponse.text();
+    return (
+        <main className="bg-black min-h-screen text-white">
+            {/* Seção de Imagem Principal */}
+            <section className="relative">
+                <img 
+                    src="cimento.jpg" 
+                    alt="Background" 
+                    className="w-full h-96 object-cover opacity-80"
+                />
+            </section>
 
-                // Usa o IP para buscar as informações geográficas
-                const geoResponse = await fetch(`http://ip-api.com/json/${ip}`);
-                const geoData = await geoResponse.json();
+            {/* Seção "Como Funciona" */}
+            <section className="py-10 text-center">
+                <h2 className="text-yellow-500 text-3xl mb-8">Como Funciona</h2>
 
-                // Exibe as informações de geolocalização no console
-                console.log("Informações de localização:", geoData);
-            } catch (error) {
-                console.error('Falha ao buscar informações geográficas:', error);
-            }
-        };
+                <div className="flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 md:space-x-6">
+                    <div className="bg-gray-800 p-6 rounded-lg shadow-md w-80 h-80 p-2 flex flex-col justify-between">
+                        <h3 className="text-2xl mb-4">Afiliado</h3>
+                        <p className="mb-4">
+                            Para o pedreiro, o processo é direto: ele cadastra suas 
+                            habilidades e localização na Fixy. Quando um cliente solicita 
+                            um serviço, a Fixy conecta as oportunidades compatíveis, 
+                            facilitando a expansão de seus clientes e negócios.
+                        </p>
+                        <Link to="afiliado" className="bg-transparent border-2 border-yellow-500 text-yellow-500  py-2 px-4 hover:tracking-widest transition-all ' : 'border-b-2 px-2 border-yellow-500  hover:tracking-widest transition-all">
+                            AFILIADO
+                        </Link>
+                    </div>
+                    <div className="bg-gray-800 p-6 rounded-lg shadow-md w-80 h-80 p-2 flex flex-col justify-between">
+                        <h3 className="text-2xl mb-4 ">Cliente</h3>
+                        <p className="mb-4">
+                            Para o cliente, é fácil: ele descreve o problema e informa sua 
+                            localização no app da Fixy. A plataforma filtra e apresenta 
+                            pedreiros disponíveis para o trabalho, simplificando a busca 
+                            por um profissional.
+                        </p>
+                        <Link to="cliente" className="bg-transparent text-black border-2 border-yellow-500 text-yellow-500 py-2 px-4 hover:tracking-widest transition-all ' : 'border-b-2 px-2 border-yellow-500  hover:tracking-widest transition-all">
+                            CLIENTE
+                        </Link>
+                    </div>
+                </div>
+            </section>
 
-        // Chama a função para obter a localização
-        fetchGeoLocation();
-    }, []);
-
-    return null; // Não renderiza nada na tela
+            {/* Seção "Solicitação de serviço" */}
+            <section className="py-10 text-center">
+                <h2 className="text-yellow-500 text-3xl mb-8">Solicitação de serviço</h2>
+                <div className="flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 md:space-x-6">
+                    <div className="text-left max-w-md">
+                        <p className="mb-4">
+                            Cadastre-se, descreva o seu problema e informe sua localização. 
+                            Nosso sistema filtrará pedreiros qualificados próximos de você, 
+                            facilitando a contratação do profissional ideal para suas necessidades.
+                        </p>
+                        <Link to="404" className="bg-yellow-500 hover:bg-yellow-400 transition-all text-black py-2 px-4">
+                            Solicitar um serviço
+                        </Link>
+                    </div>
+                    <img 
+                        src="pintor.jpg" 
+                        alt="Worker" 
+                        className="w-full md:w-1/3 h-96 object-cover bg-center opacity-80 rounded"
+                    />
+                </div>
+            </section>
+        </main>
+    );
 }
 
 export default Home;
