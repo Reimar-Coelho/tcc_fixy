fa7532c11116b597d74491c5939d7979614dd66e
Daniel <hd2242kin20@gmail.com>
2024-10-23 22:27:26 -0300

    cinquenta e dois3131313

--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -3,15 +3,14 @@ import { Link } from "react-router-dom";
 
 function Header() {
     return ( 
-        <header className="bg-transparent text-white p-3 w-full flex-wrap">
-            
-            <div className="flex flex-row items-center justify-between mx-7 felx">
+        <header className="absolute top-0 left-0 w-full text-white p-3">
+            <div className="flex flex-row items-center justify-between mx-7">
                 <h1 className="text-4xl mt-3 mb-3 text-yellow-500 font-bold tracking-tight hover:text-yellow-300 transition-all">
-                <Link to = "/">FIXY</Link></h1>
-                
+                    <Link to="/">FIXY</Link>
+                </h1>
                 <Navbar/>
            </div>
         </header>
-     );
+    );
 }
 export default Header;
--- src/Componentes/Navbar/Navbar.jsx
+++ src/Componentes/Navbar/Navbar.jsx
@@ -2,55 +2,56 @@ import {Link, useLocation} from 'react-router-dom'
 import { BsPersonCircle } from "react-icons/bs";
 
 function Navbar() {
-
     const location = useLocation();
 
     const getLoginPath = () => {
         if (location.pathname.includes('/afiliado')) {
           return '/loginParceiro';
         } 
-        else  if (location.pathname.includes('/cliente')) {
+        else if (location.pathname.includes('/cliente')) {
           return '/login';
-        }
+        } 
         else if (location.pathname.includes('/loginParceiro')){
             return '/loginParceiro'
-        }
+        } 
         else if (location.pathname.includes('/login')){
             return '/login'
-        }
-        
+        } 
         else {
             return '/escolhaLogin'
         }
-      };
+    };
 
     return ( 
-        <nav>
+        <nav className="bg-transparent z-10"> {/* Garantir que a navbar esteja acima de outros elementos */}
             <ul className='text-lg flex gap-3 ml-3 text-white flex-wrap'>
                 <li className={`${location.pathname === '/' ? 'border-b-2 px-2 border-yellow-500 text-yellow-500 hover:tracking-widest transition-all ' : ' px-2 border-yellow-500  hover:border-b-2 hover:tracking-widest hover:text-yellow-500 transition-all'}`}>
-                    <Link to = "/">HOME</Link>
+                    <Link to="/">HOME</Link>
                 </li>
 
                 <li className={`${location.pathname === '/cliente' ? 'border-b-2 px-2 border-yellow-500 text-yellow-500 hover:tracking-widest transition-all ' : ' px-2 border-yellow-500  hover:border-b-2 hover:tracking-widest hover:text-yellow-500 transition-all'}`}>
-                    <Link to = "cliente">CLIENTE</Link></li>
+                    <Link to="cliente">CLIENTE</Link>
+                </li>
 
                 <li className={`${location.pathname === '/afiliado' ? 'border-b-2 px-2 border-yellow-500 text-yellow-500 hover:tracking-widest transition-all ' : ' px-2 border-yellow-500  hover:border-b-2 hover:tracking-widest hover:text-yellow-500 transition-all'}`}>
-                    <Link to = "afiliado">AFILIADO</Link>
+                    <Link to="afiliado">AFILIADO</Link>
                 </li>
+
                 <li className={`${location.pathname === '/loginParceiro' ? 'border-b-2 px-2 border-yellow-500 text-yellow-500 hover:tracking-widest transition-all ' : ' px-2 border-yellow-500  hover:border-b-2 hover:tracking-widest hover:text-yellow-500 transition-all'}`}>
-                    <Link to = {getLoginPath()} className='flex items-center gap-2'>
-                        
+                    <Link to={getLoginPath()} className='flex items-center gap-2'>
                         LOGIN
                     </Link>
                 </li>
+
                 <li className={`${location.pathname === '/perfil' ? 'px-2 border-yellow-500 text-yellow-500 hover:tracking-widest transition-all ' : 'px-2 border-yellow-500 hover:tracking-widest hover:text-yellow-500 transition-all'}`}>
-                    <Link to = "perfil" className='flex items-center gap-2'>PERFIL
-                    <BsPersonCircle className='text-2xl text-yellow-500'/>
+                    <Link to="perfil" className='flex items-center gap-2'>
+                        PERFIL
+                        <BsPersonCircle className='text-2xl text-yellow-500'/>
                     </Link>
                 </li>
             </ul>
         </nav>
-     );
+    );
 }
 
 export default Navbar;
