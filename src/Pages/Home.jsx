8d089c3836a493b7a5af8ea0dae073fdd2ea4a24
Daniel Sanchez <hd2242kin20@gmail.com>
2024-10-28 08:42:31 -0300

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import useDocumentTitle from '../hook/useDocumentTitle';
 import { Link } from 'react-router-dom';
 
@@ -13,45 +13,67 @@ function Home() {
         targetSectionRef.current.scrollIntoView({ behavior: 'smooth' });
     };
 
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (event.key === 'Enter') {
+                scrollToSection();
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        // Limpa o evento ao desmontar
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
     return (
         <main className="bg-neutral-900 min-h-screen text-white">
             {/* Seção de Imagem Principal */}
-            <section className="relative h-screen flex justify-center items-center">
+            
+<section className="relative h-screen flex justify-center items-center">
                 <img 
                     src="cimento.jpg" 
                     alt="Background" 
                     className="absolute inset-0 w-full h-full object-cover opacity-50"
                 />
                 
-                {/* Conteúdo Centralizado */}
-                <div className="relative z-10 text-center">
-                    <img 
-                        src="whitefixy.png"  
-                        alt="Logo FIXY" 
-                        className="w-1/3 max-w-xs mx-auto transform transition-transform duration-500 hover:scale-105"
-                    />
-                    <p className="text-white text-xl mt-4">
-                        Facilitando a conexão entre clientes e profissionais
-                    </p>
+    {/* Conteúdo Centralizado */}
+    <div className="relative z-10 text-center">
+        <img 
+            src="whitefixy.png"  
+            alt="Logo FIXY" 
+            className="w-2/4 max-w-xs mx-auto transform transition-transform duration-500 hover:scale-105"
+        />
+        {/* Linha abaixo do logo */}
+    <div className="w-full h-1 bg-yellow-500 mx-auto mt-4"></div> {/* Linha */}
+    
+    <p className="text-white text-lg font-semibold mt-4">
+        CONECTANDO CLIENTES E PRESTADORES COM EFICIÊNCIA
+            </p>
                 </div>
 
                 {/* Passo 3: Ícone da Seta com onClick */}
+                <div className="absolute bottom-16 text-yellow-500 text-sm font-semibold">
+                    "Enter" para ver mais
+                </div>
                 <div
                     onClick={scrollToSection}
-                    className="absolute bottom-10 cursor-pointer text-yellow-500 hover:text-yellow-400"
+                    className="absolute bottom-4 cursor-pointer text-yellow-500 hover:text-white transition-all"
                 >
-                    <svg 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        width="40" 
-                        height="40" 
-                        fill="currentColor" 
-                        viewBox="0 0 24 24"
-                        className="animate-bounce transition-transform duration-300 hover:scale-110"
-                    >
-                        <path d="M12 19c-.256 0-.512-.098-.707-.293l-7-7c-.39-.39-.39-1.023 0-1.414s1.024-.39 1.414 0L12 16.586l6.293-6.293c.39-.39 1.024-.39 1.414 0s.39 1.024 0 1.414l-7 7c-.195.195-.451.293-.707.293z"/>
-                    </svg>
-                </div>
-            </section>
+                <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    width="40" 
+                    height="40" 
+                    fill="currentColor" 
+                    viewBox="0 0 24 24"
+                    className="animate-bounce transition-transform duration-300 hover:scale-110"
+                >   
+                <path d="M12 19c-.256 0-.512-.098-.707-.293l-7-7c-.39-.39-.39-1.023 0-1.414s1.024-.39 1.414 0L12 16.586l6.293-6.293c.39-.39 1.024-.39 1.414 0s.39 1.024 0 1.414l-7 7c-.195.195-.451.293-.707.293z"/>
+                </svg>
+            </div>
+</section>
 
             {/* Seção "Como Funciona" com referência */}
  <section ref={targetSectionRef} className="py-10 text-center">
@@ -60,7 +82,7 @@ function Home() {
         <div className="bg-neutral-800 p-8 rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl w-80 flex flex-col justify-between">
             <h3 className="text-2xl mb-4 text-yellow-500">Afiliado</h3>
             <p className="mb-6 text-gray-300">
-                Para o pedreiro, o processo é direto: ele cadastra suas 
+                Para o prestador, o processo é direto: ele cadastra suas 
                 habilidades e localização na Fixy. Quando um cliente solicita 
                 um serviço, a Fixy conecta as oportunidades compatíveis, 
                 facilitando a expansão de seus clientes e negócios.
